20b8f68822524b5b0cc4f388d96c69f36c58af2c
Jannik Nickel <nickel.jannik1997@gmail.com>
2025-03-31 21:51:23 +0200

    File locking

---
+++ bootstrapper/filelock.py
@@ -0,0 +1,50 @@
+import time
+import fcntl
+import atexit
+import signal
+
+class LockFile:
+    def __init__(self, path, timeout = None, wait_interval = 1):
+        self.path = path
+        self.timeout = timeout
+        self.wait_interval = wait_interval
+        self.file = None
+
+    def acquire(self):
+        if self.file:
+            raise RuntimeError(f"[LockFile] Lock already acquired on {self.path}")
+
+        self.file = open(self.path, "a")
+        start_time = time.time()
+
+        while True:
+            try:
+                fcntl.flock(self.file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break
+            except BlockingIOError:
+                if self.timeout and (time.time() - start_time) >= self.timeout:
+                    raise TimeoutError(f"[LockFile] Timeout while waiting to acquire lock on {self.path}")
+
+                print(f"[LockFile] Waiting for lock: {self.path}")
+                time.sleep(self.wait_interval)
+
+        atexit.register(self.release)
+        signal.signal(signal.SIGTERM, lambda *_: self.release())
+        signal.signal(signal.SIGINT, lambda *_: self.release())
+
+    def release(self):
+        try:
+            if self.file:
+                fcntl.flock(self.file.fileno(), fcntl.LOCK_UN)
+                self.file.close()
+                self.file = None
+                print(f"[LockFile] Released lock: {self.path}")
+        except Exception as e:
+            print(f"[LockFile] Error releasing lock: {e}")
+
+    def __enter__(self):
+        self.acquire()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.release()
--- bootstrapper/start.py
+++ bootstrapper/start.py
@@ -1,18 +1,28 @@
 import os
 import config
+from filelock import LockFile
+
+LOCK_TIMEOUT = 3600
 
 BOOTSTRAPPER_PATH_VAR = os.getenv("INPUT_BOOTSTRAPPER_PATH")
-BOOTSTRAPPER_PATH = os.path.abspath(os.path.join(os.getcwd(), BOOTSTRAPPER_PATH_VAR, ".env"))
+BOOTSTRAPPER_PATH = os.path.abspath(os.path.join(os.getcwd(), BOOTSTRAPPER_PATH_VAR))
+ENV_FILE_PATH = os.path.join(BOOTSTRAPPER_PATH, ".env")
+LOCK_FILE_PATH = os.path.join(BOOTSTRAPPER_PATH, ".lock")
+config.initialize(BOOTSTRAPPER_PATH)
 
 import proxmox_vm
 
-# Wait for the VM to be stopped, otherwise it is currently running a workflow
-proxmox_vm.wait_vm_status(config.RUNNER_VMID, "stopped")
+try:
+    with LockFile(LOCK_FILE_PATH, timeout = LOCK_TIMEOUT):
+        # Wait for the VM to be stopped, otherwise it is currently running a workflow
+        proxmox_vm.wait_vm_status(config.RUNNER_VMID, "stopped")
 
-# Restore the VM from the latest backup to ensure a clean state
-backup_volid = config.RUNNER_BACKUP or proxmox_vm.get_latest_backup_filename(config.RUNNER_VMID, config.BACKUP_STORAGE)
-proxmox_vm.restore_vm_backup(config.RUNNER_VMID, backup_volid)
+        # Restore the VM from the latest backup to ensure a clean state
+        backup_volid = config.RUNNER_BACKUP or proxmox_vm.get_latest_backup_filename(config.RUNNER_VMID, config.BACKUP_STORAGE)
+        proxmox_vm.restore_vm_backup(config.RUNNER_VMID, backup_volid)
 
-# Start the VM
-proxmox_vm.start_vm(config.RUNNER_VMID)
-proxmox_vm.wait_vm_status(config.RUNNER_VMID, "running")
+        # Start the VM
+        proxmox_vm.start_vm(config.RUNNER_VMID)
+        proxmox_vm.wait_vm_status(config.RUNNER_VMID, "running")
+except TimeoutError as e:
+    print(str(e))
